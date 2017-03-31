b1e449ee68902a9717de0ddfb7e89349f48dac76
Justin Broadbent <JustinB@wsecu.int>
2017-03-31 11:43:39 -0700

    Added code

---
+++ app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+
+import { EventsAppComponent } from './events-app.component'
+import { NavBarComponent } from './nav/navbar.component'
+import { ToastrService } from './common/toastr.service'
+import { appRoutes } from './routes'
+import { Error404Component } from './errors/404.component'
+import { AuthService } from './user/auth.service'
+
+
+import {
+    EventListResolver,
+    EventRouteActivator,
+    CreateEventComponent,
+    EventDetailsComponent,
+    EventService,
+    EventThumbnailComponent,
+    EventsListComponent,
+} from './events/index'
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    declarations: [EventsAppComponent, 
+        EventsListComponent, 
+        EventThumbnailComponent,
+        NavBarComponent,
+        EventDetailsComponent,
+        CreateEventComponent,
+        Error404Component
+        ],
+    providers: [
+            AuthService,
+            EventService, 
+            ToastrService, 
+            EventRouteActivator,
+            EventListResolver,
+            { 
+                provide: 'canDeactivateCreateEvent', 
+                useValue: checkDirtyState }
+        ],
+    bootstrap: [EventsAppComponent]
+})
+export class AppModule {}
+
+function checkDirtyState(component:CreateEventComponent) {
+    if(component.isDirty)
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+    return true
+}
---
+++ app/user/profile.component.ts
@@ -0,0 +1,43 @@
+import { OnInit, Component } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
+
+
+@Component({
+  templateUrl: '/app/user/profile.component.html',
+  styles: [`
+      em {float:right; color:#E05C65; padding-left: 10px;}
+      .error input {background-color:#E3C3C5;}
+      .error ::-webkit-input-placeholder { color: #999; }
+      .error ::-moz-placeholder { color: #999; }
+      .error :-moz-placeholder { color: #999; }
+      .error :ms-input-placeholder { color: #999; }
+  `]  
+})
+export class ProfileComponent implements OnInit {
+    profileForm:FormGroup
+
+    constructor(private authService:AuthService, private router:Router) {
+
+    }
+
+     ngOnInit() {
+       let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+       let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+       this.profileForm = new FormGroup({
+         firstName: firstName,
+         lastName: lastName
+       })
+     }
+     saveProfile(formValues) {
+       if (this.profileForm.valid) {
+         this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+         this.router.navigate(['events'])
+       }
+     }
+
+     cancel() {
+       this.router.navigate(['events'])
+     }
+}
